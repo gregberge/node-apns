11c4bd125bed964e55d5c62ae9174d3a770011bb
Greg Bergé <neoziro@hotmail.com>
2012-06-13 00:19:28 +0200

    add errors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.Connection = require("./lib/connection").Connection;
+exports.Errors = require("./lib/connection").Errors;
 exports.Notification = require("./lib/notification").Notification;
 exports.Device = require("./lib/notification").Device;
